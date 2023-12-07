e6119da661f854effd7242ca6cd8940f22efc311
markdavidhughes <markdhughes91@gmail.com>
2023-12-07 14:36:50 +0000

    test

--- variable.tf
+++ variable.tf
@@ -2,9 +2,4 @@ variable "location" {}
 variable "connectivityname" {}
 variable "managementname" {}
 variable "identityname" {}
-variable "alias" {
-  
-}
-variable "subscription_id" {
-  
-}
+variable "subscription_id" {}
