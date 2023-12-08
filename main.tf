80550d0dd4d34e0e41f13bea7c48beccf8857252
markdavidhughes <markdhughes91@gmail.com>
2023-12-08 11:08:41 +0000

    update

--- main.tf
+++ main.tf
@@ -3,15 +3,15 @@ module "connectivity" {
   providers = {
     azurerm = azurerm.MSDN
   }
-  location         = var.location
+  location                         = var.location
   connectivity_resource_group_name = var.connectivity_resource_group_name
-  vnet_hub_name = var.vnet_hub_name
-  vnet_hub_address_space = var.vnet_hub_address_space
-  vnet_hub_dns_space = var.vnet_hub_dns_space
-  vnet_hub_snet_name = var.vnet_hub_snet_name
-vnet_hub_snet_range = var.vnet_hub_snet_range
-nsg_name = var.nsg_name
-nsg_security_rule_rdp_name = var.nsg_security_rule_rdp_name
+  vnet_hub_name                    = var.vnet_hub_name
+  vnet_hub_address_space           = var.vnet_hub_address_space
+  vnet_hub_dns_space               = var.vnet_hub_dns_space
+  vnet_hub_snet_name               = var.vnet_hub_snet_name
+  vnet_hub_snet_range              = var.vnet_hub_snet_range
+  nsg_name                         = var.nsg_name
+  nsg_security_rule_rdp_name       = var.nsg_security_rule_rdp_name
 }
 
 module "identity" {
@@ -19,7 +19,7 @@ module "identity" {
   providers = {
     azurerm = azurerm.MSDN
   }
-  location     = var.location
+  location                     = var.location
   identity_resource_group_name = var.identity_resource_group_name
 }
 
@@ -28,6 +28,6 @@ module "management" {
   providers = {
     azurerm = azurerm.MSDN
   }
-  location       = var.location
+  location                       = var.location
   management_resource_group_name = var.management_resource_group_name
 }
--- modules/platform/connectivity/main.tf
+++ modules/platform/connectivity/main.tf
@@ -12,10 +12,10 @@ resource "azurerm_virtual_network" "vnet_hub" {
 }
 
 resource "azurerm_subnet" "vnet_hub_snet" {
-  name                = var.vnet_hub_snet_name
-  resource_group_name = azurerm_resource_group.connectivity.name
+  name                 = var.vnet_hub_snet_name
+  resource_group_name  = azurerm_resource_group.connectivity.name
   virtual_network_name = azurerm_virtual_network.vnet_hub.name
-  address_prefixes =     var.vnet_hub_snet_range
+  address_prefixes     = var.vnet_hub_snet_range
 }
 
 data "http" "clientip" {
@@ -24,20 +24,20 @@ data "http" "clientip" {
 
 resource "azurerm_network_security_group" "nsg" {
   name                = var.nsg_name
- location            = var.location
+  location            = var.location
   resource_group_name = azurerm_resource_group.connectivity.name
 
- security_rule {
-   name                       = var.nsg_security_rule_rdp_name
-   priority                   = 100
-   direction                  = "Inbound"
-   access                     = "Allow"
-   protocol                   = "*"
-   source_port_range          = "*"
-  destination_port_range     = "3389"
-   source_address_prefix      = "${chomp(data.http.clientip.body)}/32"
-   destination_address_prefix = "*"
- }
+  security_rule {
+    name                       = var.nsg_security_rule_rdp_name
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "${chomp(data.http.clientip.body)}/32"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_subnet_network_security_group_association" "vnet-hub" {
